d935a37ea0dedff1eb0e6790d2e1d9f416401e4d
nguyentrungphong <ngtrphong1980@gmail.com>
2024-06-11 11:36:51 +0700

    Phong - drawstar

---
+++ draw_star.py
@@ -0,0 +1,11 @@
+import turtle as t
+star = t.Turtle()
+star.pencolor("green")
+star.speed(10)
+for i in range(73):
+    for i in range(5):
+        star.rt(144)
+        star.fd(100)
+    star.rt(5)
+t.done()
+    
