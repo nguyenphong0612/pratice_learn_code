ec2a9dcf3b0497a3b0c858974f63e41948c1360b
nguyentrungphong <ngtrphong1980@gmail.com>
2024-06-11 16:18:14 +0700

    Phong - find 2 max number in list

---
+++ find_2num_max.py
@@ -0,0 +1,9 @@
+List = [5,4,3,2,1,0]
+smallest = List[0]
+for i in range(len(List)-1):
+    smallest = List[i]
+    for j in range(i+1, 6):
+        if List[j] < smallest:
+            smallest = List[j]
+            List[j], List[i] = List[i], smallest
+print(List[-2:])           
